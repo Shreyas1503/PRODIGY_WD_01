2e0461ab360575bfc14c67a314fc299f20d68a8a
Shreyas <126601518+Shreyas1503@users.noreply.github.com>
2024-01-28 16:56:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let divisions = document.querySelectorAll('div');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = div.offsetTop - 150;
+        let height = div.offsetHeight;
+        let id = div.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
